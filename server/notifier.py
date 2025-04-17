7491b8b6c1a32c0f139516126c5966d848bc4e2c
kevinh81760 <kevinh81760@gmail.com>
2025-04-17 15:55:44 -0700

    feat: add Telegram notification function

--- server/notifier.py
+++ server/notifier.py
@@ -1,26 +1,5 @@
-import json
-import os 
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
-# file where we save seen jobs
-SEEN_JOBS_FILE = "seen_jobs.json"
-
-def load_seen_jobs():
-    if not os.path.exists(SEEN_JOBS_FILE):
-        return []
-
-    with open(SEEN_JOBS_FILE, "r") as f:
-        try:
-            seen_jobs = json.load(f)
-        except json.JSONDecodeError:
-            seen_jobs = []
+def send_notification(message):
     
-def save_seen_jobs(job):
-    # loads current list of jobs
-    seen_jobs = load_seen_jobs()
-
-    # appeands new job to list
-    seen_jobs.append(job)
-
-    # save the updated list back to the file 
-    with open(SEEN_JOBS_FILE, "w") as file:
-        json.dump(seen_jobs, file, indent=2)
