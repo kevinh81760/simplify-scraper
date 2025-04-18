6ee85b51da48a1ee50240e43565e7d2849aad80a
kevinh81760 <kevinh81760@gmail.com>
2025-04-17 19:52:44 -0700

    feat: implemented SubmitInput shake animation

--- client/src/components/SubmitInput.jsx
+++ client/src/components/SubmitInput.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
+const shakeVariants = {
+  hidden: { x: 0 },
+  visible: {
+    x: [0, -5, 5, -5, 5, -3, 3, 0],
+    transition: {
+      duration: 0.7,
+      ease: "easeInOut"
+    }
+  }
+};
 
 const SubmitInput = () => {
-    return (
-        <button
-            type="submit"
-            className="bg-black text-white text-base rounded-2xl w-30 h-11 hover:bg-gray-900"
-        >
-            Notify Me
-        </button>
-    )
-}
+  const [startShake, setStartShake] = useState(false);
 
+  useEffect(() => {
+    setStartShake(true);
+  }, []);
 
-export default SubmitInput
+  return (
+    <motion.button
+      type="submit"
+      variants={shakeVariants}
+      initial="hidden"
+      animate={startShake ? "visible" : "hidden"}
+      className="bg-black text-white text-base rounded-2xl w-30 h-11 hover:bg-gray-900"
+    >
+      Notify Me
+    </motion.button>
+  );
+};
+
+export default SubmitInput;
