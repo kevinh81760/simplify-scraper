2baa0c9d63661432bdca32c1373e3705067e6a23
kevinh81760 <kevinh81760@gmail.com>
2025-04-21 17:34:30 -0700

    feat: notifier logic implementation

--- server/notifier.py
+++ server/notifier.py
@@ -2,4 +2,12 @@ import requests
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 def send_notification(message):
-    
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+
+    try:
+        
