3341468ec1c6d64f35cc71d47122b34e8bb032da
kevinh81760 <kevinh81760@gmail.com>
2025-04-30 21:21:54 -0700

    chore: small tweaks to existing features

--- client/src/components/PhoneField.jsx
+++ client/src/components/PhoneField.jsx
@@ -5,7 +5,7 @@ const PhoneField = () => {
             type="tel"
             placeholder="Your phone number"
             maxLength={10}
-            className="bg-white text-black text-base rounded-2xl px-3 py-3 w-105 h-14 mt-4"
+            className="bg-white text-black text-base rounded-2xl px-3 py-3 w-105 h-14 mt-4 focus:outline-none focus:ring-0"
         />
     )
 }
